671b86b52ef27ffaf75e9065030284e652fc217c
hariomaurya <hariom.maurya@unicommerce.com>
2022-02-27 15:16:19 +0530

    free api integrated

--- zomato_clone/testing.js
+++ zomato_clone/testing.js
@@ -9,16 +9,16 @@ console.log("This is my index js file");
 4. integrate free api
 */
 // Initialize the news api parameters
-let source = 'the-times-of-india';
+let source = 'us, in';
 
-let apiKey = '0921738c808f4412be7a66c95e1f20c2'
+let apiKey = '465d049e737be266180baef869fb9d12'
 
 // Grab the news container
 let newsAccordion = document.getElementById('news');
 
 // Create an ajax get request
 const xhr = new XMLHttpRequest();
-xhr.open('GET', `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`, true);
+xhr.open('GET', `http://api.mediastack.com/v1/news?access_key=${apiKey}&countries=${source}`, true);
 
 
 // What to do when response is ready
@@ -26,22 +26,24 @@ xhr.onload = function () {
     if (this.status === 200) {
         let json = JSON.parse(this.responseText);
         
-        let articles = json.articles;
+        let articles = json.data;
         let newsHtml = "";
         articles.forEach(function(element, index) {
             const monthNames = ["January", "February", "March", "April", "May", "June",
             "July", "August", "September", "October", "November", "December"
             ];
-
-            const d = new Date(element['publishedAt']);
+            const d = new Date(element['published_at']);
             var month = monthNames[d.getMonth()];
             // var dateFromApi = monthNames[d.getDate()];
-            
+            var image = "../assets/news.jpeg";
+            if(element["image"] != null){
+                image = element["image"];
+            }
             //document.write("The current month is " + monthNames[d.getMonth()]);
             let news = `<div class="ct-blog col-sm-6 col-md-4">
                             <div class="inner">
                             <div class="fauxcrop">
-                                <a href="#"><img alt="News Entry" src="${element["urlToImage"]}"></a>
+                                <a href="#"><img alt="News Entry" src="${image}"></a>
                             </div>
                                 <div class="ct-blog-content">
                                     <div class="ct-blog-date">
